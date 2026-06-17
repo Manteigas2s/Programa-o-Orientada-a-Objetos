6cd25c1a96a5d2f673a7fc373d69dfe7cadb111f
Manteigas2s <ph5154671@gmail.com>
2026-06-16 21:06:02 -0300

    Encapsulamento

--- Herancaempython/moto.py
+++ Herancaempython/moto.py
@@ -2,7 +2,7 @@ from veiculo import Veiculo
 
 class Moto(Veiculo):
     def __init__(self, modelo, marca, ano, cilindradas):
-        super().__init__(modelo, marca, ano, cilindradas)
+        super().__init__(modelo, marca, ano)
     
     def mostrar_cilindradas(self):
         print("===== CILINDRADAS DA MOTO =====")
